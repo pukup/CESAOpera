192ebfe22cfdd367185bfb02b4fcdaeefc6f1656
x <x@x-ThinkPad-X230>
2019-01-17 23:04:58 +0100

    new Heliostat.java and folders

---
+++ src/CESAOpera/mvc/model/heliostat/Heliostat.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CESAOpera.mvc.model.heliostat;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author x
+ */
+public abstract class Heliostat implements Serializable
+{
+    private final static long SerialVersionUID = 1L;
+    
+}
